07e35de1fdac49447ce3b18c226cb4adae160659
kash <kashvijayperth@gmail.com>
2023-04-07 02:34:20 +0800

    add error handling for email module

--- Email/email_reputation_checker.py
+++ Email/email_reputation_checker.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from termcolor import colored
 from Common import utils as utils
 from Common.utils import KeyFetcher
 from Common.breach_checker import BreachChecker
@@ -10,12 +11,11 @@ class EmailBreachChecker:
     def __init__(self, email):
         self.email = email
 
-    # Function to periodically download full breach data from HIBP so it can be 
+    # Method to periodically download full breach data from HIBP so it can be 
     # used to lookup further details of a breach when an IP/Email is found in the breach data
     def periodicBreachDownloader(self):
         keyfetcher = KeyFetcher()
         hibp_key = keyfetcher.getHIBPAPIKey()
-        # print('HIBPKEY in EmailBreachChecker: ', hibp_key)
         url = "https://haveibeenpwned.com/api/v3/breaches"
         payload={}
         headers = {
@@ -26,20 +26,16 @@ class EmailBreachChecker:
         try:
             response = requests.get(url, headers=headers, data=payload).text
             print("Downloading full breach data from HIBP")
-            # print("Response: ", response)
             breaches = json.loads(response)
             with open('all_breaches.json', 'w') as f:
                 json.dump(breaches, f)
         except Exception as e:
-            utils.error_message("Downloading all breaches failed",e)
+            utils.error_message(e)
 
     # Function to check email for breaches from HIBP
     def checkEmailBreach(self, email):
         keyfetcher = KeyFetcher()
         hibp_key = keyfetcher.getHIBPAPIKey()
-        # Code to check Email reputation from HIBP
-        # print("Checking Email breach from HIBP for email:", email)
-        # Check Email breach from HIBP
         url = "https://haveibeenpwned.com/api/v3/breachedaccount/"+email
         payload={}
         headers = {
@@ -47,24 +43,45 @@ class EmailBreachChecker:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Content-Type': 'application/json'
         }
-        response = requests.get(url, headers=headers, data=payload).text
-        data = json.loads(response)
-        # pretty print full json response
-        # print(json.dumps(data, indent=4, sort_keys=True))
-        for breach_name in data:
+        try:
+            response = requests.get(url, headers=headers, data=payload)
+        except Exception as e:
+            utils.error_message(e.text)
+        if response.status_code == 404:
+            print(colored("No breaches found for this email","green"))
+            exit()
+        data = json.loads(response.text)
+        # Validating the response during execution instead of wasting a call for validation
+        if 'statusCode' not in data:
+            print("Query successful")
+            for breach_name in data:
             # print("Breach name: ", breach_name['Name'])
-            with open('all_breaches.json', 'r')  as f:
-                search_name = breach_name['Name']
-                # print("Searching for breach name: ", search_name)
-                breaches = json.load(f) 
-                # print(breaches)
-                for breach in breaches:
-                    if breach['Name'] == search_name:
-                        print("\n\nAccount name:", email)
-                        print("Breach name: ", breach_name['Name'])
-                        print("Breach description: ", breach['Description'])
-                        print("Data classes that were part of this breach: ", breach['DataClasses'])
-                        print("Breach date: ", breach['BreachDate'])
-                        print("Breach is verified: ", breach['IsVerified'])
-                        print("Number of accounts compromised: ", breach['PwnCount'])
-    # return data
+                with open('all_breaches.json', 'r')  as f:
+                    search_name = breach_name['Name']
+                    # print("Searching for breach name: ", search_name)
+                    breaches = json.load(f) 
+                    # print(breaches)
+                    for breach in breaches:
+                        if 'Name' in breach and breach['Name'] == search_name:
+                            print("\n\n" + colored("Account name:", "blue"), colored(email, "red"))
+                            if 'Name' in breach:
+                                print(colored("Breach name:", "blue"), colored(breach_name['Name'], "red"))
+                            if 'Description' in breach:
+                                print(colored("Breach description:", "blue"), colored(breach['Description'], "red"))
+                            if 'BreachDate' in breach:
+                                print(colored("Breach date:", "blue"), colored(breach['BreachDate'], "white"))
+                            if 'DataClasses' in breach:
+                                print(colored("Data classes that were part of this breach:", "blue"), colored(breach['DataClasses'], "red"))
+                            if 'IsSensitive' in breach and breach['IsSensitive'] == True:
+                                print(colored("Breach is sensitive:", "blue"), colored(breach['IsSensitive'], "yellow"))
+                            if 'IsSensitive' in breach and breach['IsSensitive'] == False:
+                                print(colored("Breach is sensitive:", "blue"), colored(breach['IsSensitive'], "green"))
+                            if 'IsVerified' in breach and breach['IsVerified'] == True:
+                                print(colored("Breach is verified:", "blue"), colored(breach['IsVerified'], "green"))
+                            if 'IsVerified' in breach  and breach['IsVerified'] == False:
+                                print(colored("Breach is verified:", "blue"), colored(breach['IsVerified'], "red"))
+                            print(colored("Number of accounts compromised:", "blue"), colored(breach['PwnCount'], "white"))
+                        else:
+                            pass
+        else:
+            utils.error_message(data['message'])
