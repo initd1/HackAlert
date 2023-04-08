f4f1ee4b2f23c6241a91bd50f3f037a31f24b557
Joshua Rose <joshuarose099@gmail.com>
2023-04-08 14:51:33 +1000

    bugfix: elipses typo

--- Tests/Common/test_breach_checker.py
+++ Tests/Common/test_breach_checker.py
@@ -1,1 +1,15 @@
-...
+import unittest
+from Common.breach_checker import BreachChecker
+
+
+class BreachChecker(unittest.TestCase):
+    def setUp(self):
+        self.emails = [
+                "test@gmail.com",
+                "testgmail.com",
+                "test@@gmail",
+                "123@gmail.com.au"
+                ]
+
+    def test_is_valid_email(self):
+        self.assertTrue(self.emails[0])
