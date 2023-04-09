32d036b53cdee2599a54004919443d47763cfdbd
Joshua Rose <joshuarose099@gmail.com>
2023-04-09 13:37:44 +1000

    feat: configure logging function for modular logging

--- Config/config.py
+++ Config/config.py
@@ -11,3 +11,12 @@
 
 # # Get the Virus Total API key from the config file
 # VT_APIKey = config['APIKeys']['VT_APIKey']
+import logging.config
+
+def configure_logging(file='config.ini'):
+    """Set up default logger for logging messages.
+
+    :param `file`: File to set configuration from.
+    """
+
+    logging.config.fileConfig(file)
