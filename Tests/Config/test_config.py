e83977b4defdc2cab820e5c3e5ce7f0ef1f9a996
Joshua Rose <joshuarose099@gmail.com>
2023-04-09 20:37:31 +1000

    tests: added unit tests for logging

---
+++ Tests/Config/test_config.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+from configparser import ConfigParser
+
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.path = os.path.join('Config/logger.ini')
+
+    def test_config_exists(self):
+        self.assertTrue(self.path)
+
+    def test_config_is_valid(self):
+        parser = ConfigParser()
+        if self.test_config_exists:
+            parser.read(self.path)
+        
+        # Check if the required sections are present in the config
+        self.assertIn('formatters', parser.sections())
+        self.assertIn('handler_console', parser.sections())
+        
+        # Check if the required keys are present in the sections
+        self.assertIn('keys', parser['formatters'])
+        self.assertIn('level', parser['handler_console'])
+        
+
+if __name__ == "__main__":
+    unittest.main()
