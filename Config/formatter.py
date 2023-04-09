476273bd2cdfe817bae9b854fa37205750880dc5
Joshua Rose <joshuarose099@gmail.com>
2023-04-09 18:19:59 +1000

    feat: logging

---
+++ Config/formatter.py
@@ -0,0 +1,24 @@
+import logging
+
+
+class Formatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, levels=None):
+        super().__init__(fmt, datefmt)
+        self.levels = levels or {}
+
+    def format(self, record):
+        record.levelprefix = self.levels.get(record.levelno, '')
+        emoji = ''
+        if record.levelno == logging.CRITICAL:
+            emoji = '💣'
+        elif record.levelno == logging.ERROR:
+            emoji = '🔥'
+        elif record.levelno == logging.WARNING:
+            emoji = '⚠️'
+        elif record.levelno == logging.INFO:
+            emoji = 'ℹ️'
+        elif record.levelno == logging.DEBUG:
+            emoji = '🔍'
+        record.emoji = emoji
+        return super().format(record)
+
