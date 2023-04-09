7b44c70e3619c232ecae9ab5501a441f85aa2e63
Joshua Rose <joshuarose099@gmail.com>
2023-04-09 12:30:56 +1000

    tests: add username tests

--- Tests/IP/test_ip_reputation_checker.py
+++ Tests/IP/test_ip_reputation_checker.py
@@ -7,15 +7,12 @@ import unittest
 
 class TestIPReputationChecker(unittest.TestCase):
     def setUp(self):
-        self.ip_addrs = {
-                "malicious": [],
-                "safe": []
-                }
+        self.ip_addrs = {"malicious": [], "safe": []}
 
     def test_check_IP_reputation_talos(self):
-        """ Check IP reputation data """
+        """Check IP reputation data"""
         ...
 
     def test_check_IP_reputation_VS(self):
-        """ Check Virus Total sample data """
+        """Check Virus Total sample data"""
         ...
--- Tests/Username/test_username_reputation_checker.py
+++ Tests/Username/test_username_reputation_checker.py
@@ -1,1 +1,13 @@
-...
+"""
+TODO: populate stubs
+"""
+
+import unittest
+
+
+class TestUsernameReputation(unittest.TestCase):
+    def test_periodic_breach_downloader(self):
+        ...
+
+    def test_check_username_braech(self):
+        ...
